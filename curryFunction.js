0873edae84cf36b5fa837ca93e899e5986cfb1bc
rajon111 <mostofarajon111@gmail.com>
2023-02-23 10:24:44 +0600

    curry concept clear

--- curryFunction.js
+++ curryFunction.js
@@ -1,3 +1,28 @@
+// curry converter function
+
+function curry(func){
+    return function curried(...args){
+        if(args.length>= func.length){
+            return func.apply(this, args)
+        }else{
+            return function(...args2){
+                return curried.apply(this, args.concat(args2))
+            }
+        }
+    }
+}
+
+function sum(a, b, c){
+    return a * b * c;
+}
+
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1, 2, 3));
+console.log(curriedSum(1), (2, 3));
+console.log(curriedSum(1), (2), (3));
+
+// ........................
 function multiply (a, b, c ){
     return a * b * c ; 
 }
@@ -16,3 +41,25 @@ let step2 = step1(4);
 let step3 = step2(7);
 
 console.log(step3);
+
+// discount calculation
+
+function discount(price, disc){
+    return price - price * disc;
+}
+
+function curryDiscount(disc){
+    return (price) =>{
+        return price - price * disc;
+    };
+};
+
+let tenPercentDis = curryDiscount(0.1) //partial function
+let twentyPercentDis = curryDiscount(0.2);
+
+let customer1 = tenPercentDis(700)
+let customer2 = twentyPercentDis(1000)
+
+console.log(customer1);
+console.log(customer2);
+
